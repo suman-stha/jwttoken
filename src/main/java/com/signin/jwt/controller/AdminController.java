b77aa3f8589b78c45ce89a22b9de397ed615d893
suman-stha <sthasum5@gmail.com>
2026-04-25 20:08:54 -0400

    some validation added

--- src/main/java/com/signin/jwt/controller/AdminController.java
+++ src/main/java/com/signin/jwt/controller/AdminController.java
@@ -3,15 +3,14 @@ package com.signin.jwt.controller;
 
 import com.signin.jwt.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@Slf4j
 public class AdminController
 {
 
@@ -21,5 +20,11 @@ public class AdminController
     public ResponseEntity<String> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
         return ResponseEntity.ok("User deleted successfully");
+    }
+
+   @GetMapping("/hello")
+    public String hello() {
+     log.info("reqeust reached");
+        return "hello";
     }
 }
